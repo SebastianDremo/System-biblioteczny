e22128da744257ea23bcae4144055920f5ced4cc
Sebastian Dremo <sebastiandremo@gmail.com>
2018-05-08 23:55:56 +0200

    Login View

--- System biblioteczny/App_Start/BundleConfig.cs
+++ System biblioteczny/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace System_biblioteczny
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/myStyle.css"));
         }
     }
 }
