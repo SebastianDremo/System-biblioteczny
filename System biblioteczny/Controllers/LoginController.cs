75297b53ed533f67b98d547c57f586d2b557f827
Sebastian Dremo <sebastiandremo@gmail.com>
2018-05-14 21:35:36 +0200

    Sucesfull Login

--- System biblioteczny/Controllers/LoginController.cs
+++ System biblioteczny/Controllers/LoginController.cs
@@ -13,5 +13,17 @@ namespace System_biblioteczny.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult CheckForLogin(Loginy login)
+        {
+            var context = new durbaezgomezEntities1();
+
+            if (context.Loginy.Any(o => o.nr_karty == login.nr_karty && o.haslo == login.haslo))
+            {
+                return View("Logged", login);
+            }
+            else return View("Error");
+        }
     }
 }
